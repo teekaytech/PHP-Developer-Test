884b183c821e6e5c4c53df0f9818928470538761
teekaytech <taofeek.olalere@feghas.com>
2021-06-13 23:05:20 +0100

    Update logout path in security.yaml

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -30,8 +29,8 @@ class AuthController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout(): RedirectResponse
+    public function logout()
     {
-        return $this->redirect($this->generateUrl('app_login'));
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
